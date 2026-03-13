0a49ca2e02c64fe04c3f02eef069acd905c0b8aa
Fabio Cozza <cozzafa@gmail.com>
2026-03-13 19:52:12 +0100

    feat: widget Crisp customer support #68

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import * as Sentry from '@sentry/nextjs'
+﻿import * as Sentry from '@sentry/nextjs'
 import type { Metadata, Viewport } from 'next'
 import { CookieBanner } from '@/components/mastro/ui/CookieBanner'
 
@@ -25,8 +25,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body suppressHydrationWarning style={{ margin: 0, padding: 0, fontFamily: '-apple-system, BlinkMacSystemFont, sans-serif' }}>
         {children}
         <CookieBanner />
+        <script dangerouslySetInnerHTML={{ __html: "window.\$crisp=[];window.CRISP_WEBSITE_ID='768d7272-2c1a-4f47-8ca1-7c575519603c';(function(){d=document;s=d.createElement('script');s.src='https://client.crisp.chat/l.js';s.async=1;d.getElementsByTagName('head')[0].appendChild(s);})()" }} />
         <script dangerouslySetInnerHTML={{ __html: "if('serviceWorker' in navigator) navigator.serviceWorker.register('/sw.js')" }} />
       </body>
     </html>
   )
 }
+
