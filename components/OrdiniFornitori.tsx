268b8cf1e8f47dffb58dd390b556c65db2ddfae9
cozzafa-code <cozzafa@gmail.com>
2026-04-16 18:09:45 +0200

    Revert "fix: stub OrdiniFornitori + CostruttoreLavorazioni mancanti"
    This reverts commit e702b0268b716e6ea2ebfaf0f8e565b061c4e251.

--- components/OrdiniFornitori.tsx
+++
@@ -1,5 +0,0 @@
-﻿"use client";
-// @ts-nocheck
-export default function OrdiniFornitori({ onBack }: any) {
-  return <div style={{padding:40}}><button onClick={onBack}>Indietro</button><h2>Ordini Fornitori</h2><p>In sviluppo</p></div>;
-}
