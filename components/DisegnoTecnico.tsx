67007bac10aa7ab0dd8324656115d3e5e683590e
cozzafa-code <cozzafa@gmail.com>
2026-05-02 20:05:02 +0200

    fix(cad): zocc-free offset finale +3y -3x

--- components/DisegnoTecnico.tsx
+++ components/DisegnoTecnico.tsx
@@ -1752,8 +1752,8 @@ export default function DisegnoTecnico({ vanoId, vanoNome, vanoDisegno, realW: p
                                   const rectEl = {
                                     id: Date.now(),
                                     type: "zoccoloLibero",
-                                    x: x1,
-                                    y: yBot - TK_ZOCC * 2,
+                                    x: x1 - 3,
+                                    y: yBot - TK_ZOCC * 2 + 3,
                                     w: x2 - x1,
                                     h: TK_ZOCC * 2,
                                   };
