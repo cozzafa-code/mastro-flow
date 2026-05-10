16078539c6632b44692892834a827f8a8cd4510f
cozzafa-code <cozzafa@gmail.com>
2026-05-10 21:19:57 +0200

    fiscale: import InviaSDIModal (step 1/2)

--- components/tablet/fiscale/FiscaleTablet.tsx
+++ components/tablet/fiscale/FiscaleTablet.tsx
@@ -3,6 +3,7 @@
 import * as React from "react";
 import { supabase } from "../../../lib/supabase";
 import { getAziendaId } from "../../mastro-constants";
+import InviaSDIModal from "../modals/InviaSDIModal";
 
 type Fattura = {
   id: string;
