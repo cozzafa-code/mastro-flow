3cb21eb48666e6e8a392609cb8664157c95e662a
cozzafa-code <cozzafa@gmail.com>
2026-05-15 22:19:35 +0200

    fix: CardPianificazione usa useMastroData.aziendaId - commesse pianificazione visibili

--- components/home/CardPianificazione.tsx
+++ components/home/CardPianificazione.tsx
@@ -5,6 +5,7 @@
 
 import React, { useEffect, useState } from "react";
 import { supabase } from "@/lib/supabase";
+import { useMastroData } from "@/hooks/useMastroData";
 
 interface Props {
   aziendaId: string;
